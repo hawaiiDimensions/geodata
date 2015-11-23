0c914cae05a5ebb2aefa03ce24f9fd912546b0b0
ajrominger <ajrominger@gmail.com>
2015-11-22 21:17:01 -0700

    script to read in GPS data on sites and output standarized data (standarized names and projection) to various geospatial formats

---
+++ sites/makeSiteSpatialData.R
@@ -0,0 +1,42 @@
+library(sp)
+library(rgdal)
+setwd('~/Dropbox/hdim/sites')
+
+## function to take filename `f' and read-in GPX data, standarizing it and outputting it
+compileGPX <- function(f) {
+    ## read data and remove unneccesary fields
+    x <- readOGR(f, 'waypoints')
+    x@data <- x@data[, c('name', 'time')]
+    names(x@data) <- c('name', 'dateCreated')
+    
+    ## re-project if needed
+    if(proj4string(x) != '+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0') {
+        spTransform(x, CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
+    }
+    
+    ## standarize site names
+    site.name <- gsub('\\..*|.*/', '', f)
+    plots <- gsub('[[:alpha:]]|.*[[:space:]]|.*_|.*-', '', x@data$name)
+    plots <- paste(sapply(max(2, nchar(plots)) - nchar(plots), function(n) paste(rep('0', n), collapse='')), plots, sep='')
+    if(nchar(plots[1]) > 2 & all(substring(plots, 1, 1) == '0')) plots <- substring(plots, 2, 100)
+    x@data$name <- paste(site.name, plots, sep='_')
+    x <- x[order(x@data$name), ]
+    
+    return(x)
+}
+
+## implement function on all GPX files, binding output into one SpatialPointsDataFrame
+out <- lapply(paste('waypoints', list.files('waypoints', pattern='\\.GPX'), sep='/'), compileGPX)
+out <- do.call(rbind, out)
+out.gpx <- out
+out.gpx@data <- data.frame(name=out.gpx@data$name)
+
+## write combined data to multiple formats
+writeOGR(out.gpx, 'dimensions_plots.gpx', layer='waypoints', driver='GPX', overwrite_layer=TRUE)
+writeOGR(out, 'dimensions_plots.shp', layer='dimensions_plots', driver='ESRI Shapefile', overwrite_layer=TRUE)
+writeOGR(out, 'dimensions_plots.kml', layer='dimensions_plots', driver='KML', overwrite_layer=TRUE)
+
+setwd('~/Dropbox/hawaiiDimensions/site_selection/all_plots')
+writeOGR(out.gpx, 'dimensions_plots.gpx', layer='waypoints', driver='GPX', overwrite_layer=TRUE)
+writeOGR(out, 'dimensions_plots.shp', layer='dimensions_plots', driver='ESRI Shapefile', overwrite_layer=TRUE)
+writeOGR(out, 'dimensions_plots.kml', layer='dimensions_plots', driver='KML', overwrite_layer=TRUE)
