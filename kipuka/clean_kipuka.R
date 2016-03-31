c01ba7ad0c7a43050d73d1c330dd5f1208e81836
ajrominger <ajrominger@gmail.com>
2016-03-30 23:00:11 -0700

    trying to write gpx file

--- kipuka/clean_kipuka.R
+++ kipuka/clean_kipuka.R
@@ -1,17 +1,20 @@
 library(sp)
 library(spatstat)
 library(rgdal)
+library(rgeos)
 library(maptools)
-library(XML)
 
 setwd('~/Dropbox/hawaiidimensions/geodata/kipuka')
 
 x <- readOGR('Kipuka_NSFSelection.kml', 'Kipuka_NSFSelection')
-plot(x)
+x <- spTransform(x, CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
 
 kip <- data.frame(name=paste('kipuka', x@data$Name, sep='_'),
                   rats=gsub('</td>.*', '', gsub('.*<td>RatRemoval</td> <td>', '', x@data[, 2])),
                   area=as.numeric(gsub('</td>.*', '', gsub('.*<td>Area_ha</td> <td>', '', x@data[, 2]))))
 x@data <- kip
-x.gpx <- x
-x.gpx@data <- kip[, 1, drop=FALSE]
+
+x.gpx <- SpatialPointsDataFrame(gCentroid(x, byid=TRUE), data=kip[, 1, drop=FALSE],
+                                proj4string=CRS(prog4string(x)))
+
+writeOGR(x.gpx, 'kipuka.gpx', layer='waypoints', driver='GPX', overwrite_layer=TRUE)
