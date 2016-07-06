476d95fcfb6d78d216a5e756509c68d4a70bcf16
ajrominger <ajrominger@gmail.com>
2016-07-05 17:36:51 -0700

    got outlines of each island and made png with dpi argument to get lines right

--- maps/sites_map.R
+++ maps/sites_map.R
@@ -3,12 +3,28 @@ library(maptools)
 library(rgdal)
 library(maps)
 library(mapdata)
+library(mapproj)
 library(RColorBrewer)
+library(rgeos)
 
 ## load flow layers
 setwd('~/Dropbox/hawaiiDimensions/geoData/site_selection/Haw_St_shapefiles/Haw_St_geo_20070426_region')
 hi.geo.poly <- readOGR('.', 'Haw_St_geo_20070426_region')
-hi.geo.poly <- spTransform(hi.geo.poly, CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
+
+## get island outlines
+islands <- gUnionCascaded(hi.geo.poly)
+islands <- SpatialPolygons(list(
+    Polygons(islands@polygons[[1]]@Polygons[sapply(islands@polygons[[1]]@Polygons, 
+                                                   function(p) !p@hole & p@area > 1e+07)
+                                            ], ID=1)), 
+    proj4string = CRS(proj4string(hi.geo.poly)))
+
+## load sites
+setwd('~/Dropbox/hawaiiDimensions/geoData/sites')
+plots <- readOGR('.', 'dimensions_plots')
+plots <- spTransform(plots, CRS(proj4string(hi.geo.poly)))
+sites <- lapply(split(plots, gsub('_.*', '', plots$name)), gCentroid)
+sites <- do.call(rbind, sites)
 
 ## change wd for plotting
 setwd('~/Dropbox/hawaiiDimensions/geoData/maps')
@@ -16,7 +32,9 @@ setwd('~/Dropbox/hawaiiDimensions/geoData/maps')
 ## colors for flow ages
 geo.col <- colorRampPalette(brewer.pal(9,"YlGnBu"))(max(hi.geo.poly@data$AGE_GROUP) + 1)
 
-jpeg(filename='map_sites.jpg', width=4800*2, height=4800, type='quartz')
+png(filename='map_sites.png', width=10, height=5, units='in', res=4000)
 par(mar=rep(0, 4))
 plot(hi.geo.poly, col=geo.col[hi.geo.poly$AGE_GROUP], border=geo.col[hi.geo.poly$AGE_GROUP])
+plot(islands, add=TRUE)
+plot(sites, add=TRUE, pch=21, col='white', bg='black', cex=2)
 dev.off()
