92a434cbce46a11763b2c83eee2bb4336daa49a5
ajrominger <ajrominger@gmail.com>
2016-07-16 11:07:43 -0700

    added outline to flow age map and made background white

--- maps/flowAge_map.R
+++ maps/flowAge_map.R
@@ -9,15 +9,21 @@ library(RColorBrewer)
 setwd('~/Dropbox/hawaiiDimensions/geoData/site_selection/Haw_St_shapefiles/Haw_St_geo_20070426_region')
 
 hi.geo.poly <- readOGR('.', 'Haw_St_geo_20070426_region')
-
-ka.geo <- hi.geo.poly[hi.geo.poly$ISLAND=='Kauai', ]
-ma.geo <- hi.geo.poly[hi.geo.poly$ISLAND=='Maui', ]
 hi.geo <- hi.geo.poly[hi.geo.poly$ISLAND=='Hawaii', ]
+
+## get island outline
+island <- gUnionCascaded(hi.geo)
+island <- SpatialPolygons(list(
+    Polygons(island@polygons[[1]]@Polygons[sapply(island@polygons[[1]]@Polygons, 
+                                                   function(p) !p@hole & p@area > 1e+07)
+                                            ], ID=1)), 
+    proj4string = CRS(proj4string(hi.geo)))
 
 ## colors for flow ages
 geo.col <- colorRampPalette(brewer.pal(9,"YlGnBu"))(max(hi.geo.poly@data$AGE_GROUP) + 1)
 
-jpeg(filename='../../../maps/map_hawaii_flowAge.jpg', bg='black')
-par(mar=rep(0, 4), bg='black')
+jpeg(filename='../../../maps/map_hawaii_flowAge.jpg', width=4, height=4, units='in', res=400)
+par(mar=rep(0, 4))
 plot(hi.geo, col=geo.col[hi.geo$AGE_GROUP], border=geo.col[hi.geo$AGE_GROUP])
+plot(island, add=TRUE)
 dev.off()
